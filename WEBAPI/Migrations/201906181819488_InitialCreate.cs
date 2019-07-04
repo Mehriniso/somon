81038fd9327c9374883733c25a2a1e6cb6bdf6db
Mehriniso Kamalova <mehrinisokamalova@marun.edu.tr>
2019-07-04 04:02:01 +0300

    Second Commit

---
+++ WEBAPI/Migrations/201906181819488_InitialCreate.cs
@@ -0,0 +1,45 @@
+namespace WEBAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tasks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TaskName = c.String(nullable: false),
+                        Explanation = c.String(),
+                        ExpirationDate = c.DateTime(),
+                        UpdatedDate = c.DateTime(),
+                        Category_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.Category_Id, cascadeDelete: true)
+                .Index(t => t.Category_Id);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        UpdatedDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tasks", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Tasks", new[] { "Category_Id" });
+            DropTable("dbo.Categories");
+            DropTable("dbo.Tasks");
+        }
+    }
+}
